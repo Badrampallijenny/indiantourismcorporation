67edcdbff706140c34090ff2b601f28f3e8b8890
Badrampallijenny <66950719+Badrampallijenny@users.noreply.github.com>
2020-08-16 20:46:03 +0530

    indiantourismcorporation

---
+++ indiantourismcorporation/src/main/java/com/dxc/controller/IndiantourismcorporationApplication.java
@@ -0,0 +1,13 @@
+package com.dxc.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndiantourismcorporationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndiantourismcorporationApplication.class, args);
+	}
+
+}
